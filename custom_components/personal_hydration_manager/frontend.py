99716d90e16df96602b488ae0e52dcac937271fc
Loryan Strant <51473494+loryanstrant@users.noreply.github.com>
2026-05-16 14:21:57 +0000

    Add files via upload

--- custom_components/personal_hydration_manager/frontend.py
+++ custom_components/personal_hydration_manager/frontend.py
@@ -25,7 +25,7 @@ from homeassistant.core import HomeAssistant
 
 _LOGGER = logging.getLogger(__name__)
 
-CARD_VERSION = "0.1.4"
+CARD_VERSION = "0.1.6"
 STATIC_URL_BASE = "/personal_hydration_manager_static"
 CARD_FILE = "personal-hydration-card.js"
 CARD_URL = f"{STATIC_URL_BASE}/{CARD_FILE}"
